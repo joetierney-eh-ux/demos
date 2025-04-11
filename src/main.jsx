0f0aabab7c8c770fef5723095b80ec8ed6a3c61b
joetierney-eh-ux <joe.tierney@experian.com>
2025-04-11 17:19:13 -0500

    updating main for local dev

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { HashRouter, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from './components/ThemeContext';
 import ThemeWrapper from './components/ThemeWrapper';
 import ThemeToggle from './components/ThemeToggle';
@@ -16,6 +17,7 @@ createRoot(document.getElementById('root')).render(
   <ThemeProvider>
     <ThemeWrapper>
       <BrowserRouter>
+      {/* <HashRouter> */}
         <Routes>
           <Route path="/demos" element={<App />} />
           <Route path="/demos/date-of-birth" element={<DOB_All />} />
@@ -23,8 +25,15 @@ createRoot(document.getElementById('root')).render(
           <Route path="/demos/time-input" element={<Time_1 />} />
           <Route path="/demos/time-select" element={<TimeSelect />} />
           <Route path='/demos/thanks' element={<ThanksPage/>} />
+          {/* <Route path="/" element={<App />} />
+          <Route path="/date-of-birth" element={<DOB_All />} />
+          <Route path="/date-range" element={<StartEnd_All />} />
+          <Route path="/time-input" element={<Time_1 />} />
+          <Route path="/time-select" element={<TimeSelect />} />
+          <Route path='/thanks' element={<ThanksPage/>} /> */}
         </Routes>
       </BrowserRouter>
+      {/* </HashRouter> */}
       <ThemeToggle/>
     </ThemeWrapper>
   </ThemeProvider>,
